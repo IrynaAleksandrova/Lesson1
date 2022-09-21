596b9f8baf6507b442457dd18d384da737294c01
Iryna Lytkina <lytkina.iryna@gmail.com>
2022-09-21 09:48:10 +0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ public class Main {
     public static void main(String[] args) {
         int a = 15;
         int b = 8;
-        System.out.println(a+b);
-        System.out.println("Hello world!");
+        int c = 77;
+        System.out.println(a+b*c);
+        System.out.println("Hello world");
     }
 }
